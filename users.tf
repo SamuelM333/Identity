c7925b5de37580713d077fef463e2e4f24ab70e2
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-20 12:11:09 -0500

    Add IAM users

---
+++ users.tf
@@ -0,0 +1,21 @@
+module "samuel_murillo" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "~> 4.7"
+
+  name          = "samuel-murillo"
+  force_destroy = false
+
+  pgp_key = "keybase:samuelm333"
+
+  password_reset_required = false
+}
+
+module "reviewer" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "~> 4.7"
+
+  name          = "reviewer"
+  force_destroy = false
+
+  password_reset_required = false
+}
