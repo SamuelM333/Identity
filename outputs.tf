5ddf364f0e4f00af604f27bd2a121032c93eb93f
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-20 12:11:47 -0500

    Output IAM resources

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "samuel_murillo" {
+  value = module.samuel_murillo
+}
+
+output "reviewer" {
+  value = module.reviewer
+}
+
+output "admin_role_arn" {
+  value = module.admin_role.iam_role_arn
+}
