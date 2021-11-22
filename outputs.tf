684cf59e6ae7d0103697b41ede49b98ee33202c4
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-22 11:57:45 -0500

    add keys output

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,20 @@
 output "samuel_murillo" {
-  value     = module.samuel_murillo
-  sensitive = true
+  value = {
+    iam_access_key_id : module.samuel_murillo.iam_access_key_id,
+    iam_access_key_encrypted_secret : module.samuel_murillo.iam_access_key_encrypted_secret,
+    iam_user_login_profile_encrypted_password : module.samuel_murillo.iam_user_login_profile_encrypted_password,
+    keybase_secret_key_decrypt_command : module.samuel_murillo.keybase_secret_key_decrypt_command
+
+  }
 }
 
 output "reviewer" {
-  value     = module.reviewer
-  sensitive = true
-
+  value = {
+    iam_access_key_id : module.reviewer.iam_access_key_id,
+    iam_access_key_encrypted_secret : module.reviewer.iam_access_key_encrypted_secret,
+    iam_user_login_profile_encrypted_password : module.reviewer.iam_user_login_profile_encrypted_password
+    keybase_secret_key_decrypt_command : module.reviewer.keybase_secret_key_decrypt_command
+  }
 }
 
 output "admin_role_arn" {
