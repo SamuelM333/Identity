3d68d2c09d093ebf877efb751dfb782dbf7cafc8
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-22 12:08:42 -0500

    add provider

--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
+variable "region" {
+  type = string
+  description = "AWS region"
+  default = "us-east-1"
+}
+
 variable "principal" {
+  type = string
   description = "ARN of the principal account"
 }
